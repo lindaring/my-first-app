a83bdd2de541be396aac40e78e710b98fb38f709
Lindaring <lndsimelane@gmail.com>
2019-09-21 17:38:28 +0200

    Added ngStyle to change css style dynamically

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,10 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  STATUS_ONLINE = 'online';
+  STATUS_OFFLINE = 'offline';
+
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+
+  constructor() {
+    this.serverStatus = Math.random() > .5 ? this.STATUS_OFFLINE : this.STATUS_ONLINE;
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColour() {
+    return this.serverStatus === this.STATUS_ONLINE ? 'lightgreen' : 'pink';
   }
 }
