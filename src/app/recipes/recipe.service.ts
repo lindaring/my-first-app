50f3f73f12dbc19f4df3fa5f4ae17b955b25f506
lindaring91 <linda.simelane@proagrica.com>
2019-09-27 14:40:15 +0200

    Added ingredient to Recipes output

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,17 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/Ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is simply a test',
-      'https://www.readyseteat.com/sites/g/files/qyyrlu501/files/uploadedImages/img_4201_1466.jpg'),
+      'https://www.readyseteat.com/sites/g/files/qyyrlu501/files/uploadedImages/img_4201_1466.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
     new Recipe('Another test recipe', 'This is simply a test',
-      'https://www.readyseteat.com/sites/g/files/qyyrlu501/files/uploadedImages/img_4201_1466.jpg')
+      'https://cdn.pixabay.com/photo/2014/07/08/12/34/pizza-386717__340.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)])
   ];
 
   getRecipies() {
