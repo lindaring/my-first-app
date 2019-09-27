8b5f3263f27984dfd77528be228a7ec7c9b7c1b6
lindaring91 <linda.simelane@proagrica.com>
2019-09-27 11:32:22 +0200

    Can now select recipe from RecipeService

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is simply a test',
       'https://www.readyseteat.com/sites/g/files/qyyrlu501/files/uploadedImages/img_4201_1466.jpg'),
