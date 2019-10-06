73916bba1124374a6286acf9da78be9d24bbf7ca
Lindaring <lndsimelane@gmail.com>
2019-10-06 08:46:49 +0200

    Form: Able to select and edit an ingredient in shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -35,7 +35,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.shoppoinglistService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.shoppoinglistService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.shoppoinglistService.addIngredient(newIngredient);
+    }
   }
 
   ngOnDestroy(): void {
